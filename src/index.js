e05af647283c5dbab7091647e73804648c62fd3b
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2017-11-30 21:50:41 +0000

    add icons to header, re-structure files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/app.css';
-import App from './App';
+import App from './scripts/components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -1,4 +1,8 @@
 import React  from 'react';
+import supportIcon from '../../imgs/ico-support.png';
+import mouseIcon from '../../imgs/ico-mouse.png';
+import drmIcon from '../../imgs/ico-drm.png';
+import timeIcon from '../../imgs/ico-time.png';
 
 class App extends React.Component {
   render() {
@@ -6,12 +10,20 @@ class App extends React.Component {
       <div className="page">
         <header className="header container">
           <h1 className="title">Pay what you want for <strong>Divinity Bundle</strong> ($32 value!)</h1>
-          <ul className="details flex justify-evenly padding30">
-            <li>Support Larian Studios</li>
-            <li>PLay Divinity 2: DC before release</li>
-            <li>Get DRM-free games with goodies</li>
-            <li className="highlight">Only <strong className="regular">23:54:55</strong> left</li>
-          </ul>
+          <div className="details flex justify-evenly">
+            <div className="display-table">
+              <div className="display-cell"><img src={supportIcon} alt="Support Larian Studios" className="icon" /><span className="text">Support Larian Studios</span></div>
+            </div>
+            <div className="display-table">
+              <div className="display-cell"><img src={mouseIcon} alt="Play Divinity 2: DC before release" className="icon" /><span className="text">PLay Divinity 2: DC before release</span></div>
+            </div>
+            <div className="display-table">
+              <div className="display-cell"><img src={drmIcon} alt="Get DRM-free games with goodies" className="icon" /><span className="text">Get DRM-free games with goodies</span></div>
+            </div>
+            <div className="highlight display-table">
+              <div className="display-cell"><img src={timeIcon} alt="Only 23:54:55 left" className="icon" /><span className="text">Only <strong className="regular">23:54:55</strong> left</span></div>
+            </div>
+          </div>
         </header>
         <main className="main container no-padding">
           <div className="games padding30">
