ac259c4e7ac31a76ddfdfc38a0b32058e609618f
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2017-11-30 23:28:46 +0000

    fix justify-evenly on ie11

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -12,17 +12,17 @@ class App extends React.Component {
       <div className="page">
         <header className="header container">
           <h1 className="title">Pay what you want for <strong>Divinity Bundle</strong> ($32 value!)</h1>
-          <div className="details flex justify-evenly">
-            <div className="display-table">
+          <div className="details flex">
+            <div className="detail display-table">
               <div className="display-cell"><img src={supportIcon} alt="Support Larian Studios" className="icon" /><span className="text">Support Larian Studios</span></div>
             </div>
-            <div className="display-table">
+            <div className="detail display-table">
               <div className="display-cell"><img src={mouseIcon} alt="Play Divinity 2: DC before release" className="icon" /><span className="text">PLay Divinity 2: DC before release</span></div>
             </div>
-            <div className="display-table">
+            <div className="detail display-table">
               <div className="display-cell"><img src={drmIcon} alt="Get DRM-free games with goodies" className="icon" /><span className="text">Get DRM-free games with goodies</span></div>
             </div>
-            <div className="highlight display-table">
+            <div className="detail highlight display-table">
               <div className="display-cell"><img src={timeIcon} alt="Only 23:54:55 left" className="icon" /><span className="text">Only <strong className="regular">23:54:55</strong> left</span></div>
             </div>
           </div>
