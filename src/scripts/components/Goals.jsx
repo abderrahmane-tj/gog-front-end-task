ca9158eda85f0464cd1477dca9deb2c2c66d10ad
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2017-12-01 23:44:31 +0000

    add games sold digits

--- src/scripts/components/Goals.jsx
+++ src/scripts/components/Goals.jsx
@@ -3,6 +3,17 @@ export default function Goals() {
   return (
     <div className="goals">
       <div className="section-title"><span>Games sold so far</span></div>
+      <div className="games-sold">
+        <div className="number">
+          <div className="digit">0</div>
+          <div className="digit">2</div>
+          <div className="digit">2</div>
+          <div className="dot-wrapper"><div className="dot" /></div>
+          <div className="digit">5</div>
+          <div className="digit">7</div>
+          <div className="digit">6</div>
+        </div>
+      </div>
     </div>
   );
 }
