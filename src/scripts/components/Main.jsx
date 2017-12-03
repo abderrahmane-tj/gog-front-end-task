ab5174577a3e4c58547924da96465f0a64b736a1
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2017-12-03 13:48:01 +0000

    add Dynamic logic to Game component

--- src/scripts/components/Main.jsx
+++ src/scripts/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 import errorIcon from '../../imgs/error.png';
 import checkIcon from '../../imgs/check.png';
 import ddIcon from '../../imgs/dd.png';
@@ -13,35 +14,30 @@ export default function Main() {
     <main className="main container no-padding">
       <div className="games padding30">
         <div className="flex">
-          <div className="game dd item active">
-            <div className="logo display-table">
-              <div className="display-cell">
-                <img className="primary" src={ddIcon} alt="" />
-                <img className="secondary" src={ddInactiveIcon} alt=""/>
-              </div>
-            </div>
-            <div className="description">
-              <p><em>Divine Divinity</em> (normal price $5.99)<br /><strong>with 6 goodies and 4 language versions</strong></p>
-            </div>
-            <div className="indication active display-table"><div className="display-cell"><img className="icon" src={checkIcon} alt="Active"/><span className="text">Below average</span></div></div>
-          </div>
-          <div className="game bd item active">
-            <div className="logo display-table"><div className="display-cell"><img className="primary" src={bdIcon} alt=""/><img className="secondary" src={bdInactiveIcon} alt=""/></div></div>
-            <div className="description">
-              <p><em>Beyond Divinity</em> (normal price $5.99)<br /><strong>with 6 goodies and 4 language versions</strong></p>
-            </div>
-            <div className="indication active display-table">
-              <div className="display-cell"><img className="icon" src={checkIcon} alt="Active"/><span className="text">Above average <strong>(from $7.67)</strong></span></div>
-            </div>
-          </div>
-          <div className="game d2 item">
-            <div className="logo display-table"><div className="display-cell"><img className="primary" src={d2Icon} alt=""/><img className="secondary" src={d2InactiveIcon} alt=""/></div></div>
-            <div className="description">
-              <p><em>Divinity 2</em> (preorder, normal price $19.99)<br /><strong>with 9 goodies and 7 language versions</strong></p>
-            </div>
-            <div className="indication display-table">
-              <div className="display-cell"><img className="icon" src={errorIcon} alt="Active"/><span className="text">Top supporter <strong>(from $18.31)</strong></span></div></div>
-          </div>
+          <Game
+            className="dd"
+            active={true}
+            primary={ddIcon}
+            secondary={ddInactiveIcon}
+            description={<p><em>Divine Divinity</em> (normal price $5.99)<br /><strong>with 6 goodies and 4 language versions</strong></p>}
+            indication={<span className="text">Below average</span>}
+          />
+          <Game
+            className="bd"
+            active={true}
+            primary={bdIcon}
+            secondary={bdInactiveIcon}
+            description={<p><em>Beyond Divinity</em> (normal price $5.99)<br /><strong>with 6 goodies and 4 language versions</strong></p>}
+            indication={<span className="text">Above average <strong>(from $7.67)</strong></span>}
+          />
+          <Game
+            className="d2"
+            active={false}
+            primary={d2Icon}
+            secondary={d2InactiveIcon}
+            description={<p><em>Divinity 2</em> (preorder, normal price $19.99)<br /><strong>with 9 goodies and 7 language versions</strong></p>}
+            indication={<span className="text">Top supporter <strong>(from $18.31)</strong></span>}
+          />
         </div>
       </div>
       <div className="choose-your-price padding30">
@@ -63,5 +59,28 @@ export default function Main() {
         </div>
       </div>
     </main>
+  );
+}
+
+function Game(props) {
+  const {
+    className, active, primary, secondary, description, indication
+  } = props;
+  return (
+    <div className={cx("game item", className, { active })}>
+      <div className="logo display-table">
+        <div className="display-cell">
+          <img className="primary" src={primary} alt="" />
+          <img className="secondary" src={secondary} alt=""/>
+        </div>
+      </div>
+      <div className="description">{description}</div>
+      <div className="indication display-table">
+        <div className="display-cell">
+          <img className="icon" alt=""
+            src={active ? checkIcon : errorIcon}
+          />{indication}</div>
+      </div>
+    </div>
   );
 }
